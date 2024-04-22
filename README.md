89d8e814bf97dabfc086c435544a37174026f7f2
azamatyashinov <azamatyashinovv@gmail.com>
2024-04-22 11:31:54 -0700

    iudhiudho

---
+++ README.md
@@ -0,0 +1,15 @@
+#                    Lesson 38
+
+ - Data types (Ma'lumotlar turlari) ikki turga  
+   bo'linadi.
+    -- 1. Pirimitiv    2. Complex
+          null            object
+          undefined
+          boolean
+          number
+          bigint
+          string
+          symbol
+          
+   
+       
---
+++ main.js
@@ -0,0 +1,29 @@
+//  Lesson 38
+
+
+// 1-masala
+
+// let A;
+// let B;
+// let Y;
+// A = Number(prompt("A sonni kiriting__"))
+// B = Number(prompt("B sonni kiriting__"))
+// Y=(-B/2)*(A**2 + B**2)
+// console.log("Javob: "+ Y)
+
+// 2-masala
+
+// let A;
+// let B;
+// let C;
+// let D;
+// let Y;
+// A = Number(prompt("A sonni kiriting__"))
+// B = Number(prompt("B sonni kiriting__"))
+// C = Number(prompt("C sonni kiriting__"))
+// D = Number(prompt("D sonni kiriting__"))
+// Y=((A+B)/(C+D))**2+B**2
+// console.log("Javob: "+ Y)
+
+// 3-masala
+
