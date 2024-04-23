6913a0836c524a0765672d3a82dfc5e72b7c74ce
azamatyashinov <azamatyashinovv@gmail.com>
2024-04-22 19:38:03 -0700

    iuipok

--- main.js
+++ main.js
@@ -27,3 +27,111 @@
 
 // 3-masala
 
+// let A;
+// let B;
+// let C;
+// let D;
+// let Y;
+// A = Number(prompt("A sonni kiriting__"));
+// B = Number(prompt("B sonni kiriting__"));
+// C = Number(prompt("C sonni kiriting__"));
+// D = Number(prompt("D sonni kiriting__"));
+// Y = 2*(A**D +((A*C)**2)/3);
+// console.log("Javob: "+ Y);
+
+// 4-masala
+
+// let A;
+// let B;
+// let C;
+// let D;
+// let Y;
+// A = Number(prompt("A sonni kiriting__"))
+// B = Number(prompt("B sonni kiriting__"))
+// C = Number(prompt("C sonni kiriting__"))
+// D = Number(prompt("D sonni kiriting__"))
+// Y= (1/(A**2))*((B/10)**2)*((C+D)**2)
+// console.log("Javob: "+ Y)
+
+// 5-masala
+
+// let A;
+// let B;
+// let C;
+// let D;
+// let Y;
+// A = Number(prompt("A sonni kiriting__"))
+// B = Number(prompt("B sonni kiriting__"))
+// C = Number(prompt("C sonni kiriting__"))
+// D = Number(prompt("D sonni kiriting__"))
+// Y= ((A/B-1))/(C/(D-1))
+// console.log("Javob: "+ Y)
+
+// 6-masala
+
+// let A;
+// let B;
+// let C;
+// let D;
+// let Y;
+// A = Number(prompt("A sonni kiriting__"))
+// B = Number(prompt("B sonni kiriting__"))
+// C = Number(prompt("C sonni kiriting__"))
+// Y=(A+B)/(C+D/(A+C))
+// console.log("Javob: "+ Y)
+
+
+// 7-masala
+
+// let A;
+// let B;
+// let C;
+// let D;
+// let Y;
+// A = Number(prompt("A sonni kiriting__"))
+// B = Number(prompt("B sonni kiriting__"))
+// C = Number(prompt("C sonni kiriting__"))
+// Y=((A+B+C)**2-(A-B-C)**2)**(1/2)
+// console.log("Javob: "+ Y)
+
+
+// 8-masala
+
+
+// let A;
+// let B;
+// let C;
+// let D;
+// let Y;
+// A = Number(prompt("A sonni kiriting__"))
+// B = Number(prompt("B sonni kiriting__"))
+// C = Number(prompt("C sonni kiriting__"))
+// Y= (A**2+ B**2 +C**2)/(A*B*C)
+// console.log("Javob: "+ Y)
+
+// 9-masala
+
+// let A;
+// let B;
+// let C;
+// let D;
+// let Y;
+// A = Number(prompt("A sonni kiriting__"))
+// B = Number(prompt("B sonni kiriting__"))
+// C = Number(prompt("C sonni kiriting__"))
+// Y=A*(D**3/3+B*(C**2/2))
+// console.log("Javob: "+ Y)
+
+// 10-masala
+
+
+// let A;
+// let B;
+// let C;
+// let D;
+// let Y;
+// A = Number(prompt("A sonni kiriting__"))
+// B = Number(prompt("B sonni kiriting__"))
+// C = Number(prompt("C sonni kiriting__"))
+// Y= Math.abs((A/B-B/C))+(A+C)*(1/4)
+// console.log("Javob: "+ Y)
